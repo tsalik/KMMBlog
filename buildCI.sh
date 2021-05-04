cfd6a1b9ccfdda5f5a91e7463538c3eab946938a
Kostas Tsalikis <tsalikispk@gmail.com>
2021-05-04 13:57:53 +0300

    Amend build script to build and test both Android and iOS

--- buildCI.sh
+++ buildCI.sh
@@ -1,2 +1,7 @@
 #!/bin/zsh
+# Build and test(unit and instrumentation) for Android
 ./gradlew clean build :androidApp:connectedAndroidTest
+
+# Build and test for iOS
+xcodebuild build-for-testing -project iosApp/iosApp.xcodeproj -scheme iosAppUITests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.4'
+xcodebuild test-without-building -project iosApp/iosApp.xcodeproj -scheme iosAppUITests -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.4'
