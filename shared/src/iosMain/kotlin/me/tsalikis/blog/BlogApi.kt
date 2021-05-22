0c13cea54d98c26d13b074a49008d9dda5668863
Kostas Tsalikis <tsalikispk@gmail.com>
2021-05-22 21:32:45 +0300

    Add kotlinx serialization for iOS HTTP client, Add Embassy to act as the server for iOS tests

--- shared/src/iosMain/kotlin/me/tsalikis/blog/BlogApi.kt
+++ shared/src/iosMain/kotlin/me/tsalikis/blog/BlogApi.kt
@@ -2,6 +2,15 @@ package me.tsalikis.blog
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.ios.Ios
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import kotlinx.serialization.json.Json
 
 
-internal actual val httpClient: HttpClient = HttpClient(Ios)
+internal actual val httpClient: HttpClient = HttpClient(Ios) {
+    install(JsonFeature) {
+        serializer = KotlinxSerializer(Json {
+            prettyPrint = true
+        })
+    }
+}
