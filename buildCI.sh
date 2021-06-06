6c245a58846662d910898fb28417d745813c7fb8
Kostas Tsalikis <tsalikispk@gmail.com>
2021-06-06 17:46:18 +0300

    Fix running iOS simulator end-to-end tests through workspace

--- buildCI.sh
+++ buildCI.sh
@@ -1,7 +1,7 @@
-#!/bin/zsh
+#!/bin/sh
 # Build and test(unit and instrumentation) for Android
-./gradlew clean build :androidApp:connectedAndroidTest
+./gradlew clean :androidApp:connectedAndroidTest
 
 # Build and test for iOS
-xcodebuild build-for-testing -project iosApp/iosApp.xcodeproj -scheme iosAppUITests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.4'
-xcodebuild test-without-building -project iosApp/iosApp.xcodeproj -scheme iosAppUITests -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.4'
+xcodebuild build-for-testing -workspace iosApp/iosApp.xcworkspace -scheme iosAppUITests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.4'
+xcodebuild test-without-building -workspace iosApp/iosApp.xcworkspace -scheme iosAppUITests -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.4'
